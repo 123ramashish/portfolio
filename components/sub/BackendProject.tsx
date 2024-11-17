db59f26b0d5a25000cacb665a9e73b8ac0259716
RAM ASHISH KUMAR <112244101+123ramashish@users.noreply.github.com>
2024-11-17 22:09:28 +0530

    Update BackendProject.tsx

--- components/sub/BackendProject.tsx
+++ components/sub/BackendProject.tsx
@@ -3,13 +3,25 @@ import ProjectCard from './ProjectCard';
 const BackendProject = () => {
   return (
     <>
-      <div className="flex flex-wrap gap-8 m-auto justify-center z-30">
+      <div className="flex flex-wrap gap-8 m-auto justify-center z-30 px-12">
         <ProjectCard
-          src="/alipublicschool.png"
-          title="Ali Public School"
-          description="A comprehensive school management system designed using WordPress, featuring essential tools for efficiently managing academic and administrative tasks, including real-time analytics and custom reporting features."
-          link="https://alipublicschool.online/"
+          src="/mvnfmsimage.png"
+          title="MVNFMS Solar System"
+          description="An advanced solar management system built with WordPress, offering real-time energy analytics, performance monitoring, and custom reporting for efficient and sustainable energy optimization."
+          link="https://mvnfms.com/"
         />
+        <ProjectCard
+          src="/liorakimage.png"
+          title="E-Commerce Managment"
+          description="An advanced e-commerce management system enabling seamless product management, order tracking, inventory control, and real-time analytics for enhanced user experience and operational efficiency."
+          link="https://liorak.in/"
+        />
+         <ProjectCard
+        src="/cleanup.png"
+        title="Samasya-Samadhan"
+        description="Samasya-Samadhan is an environmental startup project aimed at promoting clean and green initiatives. It addresses pollution and environmental issues through community-driven solutions."
+        link="https://samasya-samadhan.onrender.com"
+      />
         <ProjectCard
           src="/blogapp.png"
           title="Technology Blog App"
@@ -22,6 +34,7 @@ const BackendProject = () => {
           description="A real-time location tracking application developed to monitor and visualize user movements on a map, enhancing user interaction and data accuracy."
           link="https://track-me.onrender.com"
         />
+        
       </div>
     </>
   );
